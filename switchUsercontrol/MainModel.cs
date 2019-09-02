062e478f90703707288e6795069737c4a08b49f3
hwjung <hwjung@zinnotech.com>
2019-09-02 10:34:50 +0900

    first commit

---
+++ switchUsercontrol/MainModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows.Input;
+
+namespace switchUsercontrol
+{
+    public class MainModel : ViewModelBase
+    {
+        private int switchView;
+        public int SwitchView
+        {
+            get
+            {
+                return switchView;
+            }
+            set
+            {
+                switchView = value;
+                OnPropertyChanged("SwitchView");
+            }
+        }
+
+        public ICommand SwitchViewCommand { get; }
+
+        public MainModel()
+        {
+            SwitchView = 0;
+
+            SwitchViewCommand = new RelayCommand<object>(p => OnSwitchView(p));
+        }
+
+        private void OnSwitchView(object index)
+        {
+            SwitchView = int.Parse(index.ToString());
+        }
+    }
+}
---
+++ switchUsercontrol/ViewModelBase.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace switchUsercontrol
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
